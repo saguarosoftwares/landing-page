caf6c29a5ef9f7d0cf48823c4d14f4414be6e754
aghiulez <ara.is.dev@gmail.com>
2024-06-10 09:07:39 -0700

    baseline updates

--- src/pages/Home/useBackgroundColor.tsx
+++ src/pages/Home/useBackgroundColor.tsx
@@ -6,20 +6,35 @@ interface Section {
 }
 
 const useBackgroundColor = (sections: Section[]) => {
-  const [backgroundColor, setBackgroundColor] = useState<string>("#597b73");
+  const [backgroundColor, setBackgroundColor] = useState<string>("transparent");
+  const [isNavbarFixed, setIsNavbarFixed] = useState<boolean>(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const sectionInView = sections.find(({ id }) => {
-        const element = document.getElementById(id);
-        if (!element) return false;
-        const rect = element.getBoundingClientRect();
-        return rect.top <= window.innerHeight && rect.bottom >= 0;
-      });
+      const navbar = document.getElementById("navbar");
 
-      if (sectionInView) {
-        console.log("sectionInView: ", sectionInView)
-        setBackgroundColor(sectionInView.color);
+      if (navbar) {
+        const navbarIsFixed = window.getComputedStyle(navbar).position === "fixed";
+
+        if (navbarIsFixed && !isNavbarFixed) {
+          setIsNavbarFixed(true);
+        } else if (!navbarIsFixed && isNavbarFixed) {
+          setIsNavbarFixed(false);
+          setBackgroundColor("transparent");
+        }
+
+        if (navbarIsFixed) {
+          const sectionInView = sections.find(({ id }) => {
+            const element = document.getElementById(id);
+            if (!element) return false;
+            const rect = element.getBoundingClientRect();
+            return rect.top <= window.innerHeight && rect.bottom >= 0;
+          });
+
+          if (sectionInView) {
+            setBackgroundColor(sectionInView.color);
+          }
+        }
       }
     };
 
@@ -29,12 +44,27 @@ const useBackgroundColor = (sections: Section[]) => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [sections]);
+  }, [sections, isNavbarFixed]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = backgroundColor;
+  }, [backgroundColor]);
 
 
   useEffect(() => {
-    // Update the background color of the root element
-    document.documentElement.style.backgroundColor = backgroundColor;
+    let backgroundColor = document.body.style.backgroundColor;
+    if (backgroundColor == "transparent") {
+      backgroundColor = "#fff"
+    }
+    const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+    if (themeColorMetaTag) {
+      themeColorMetaTag.setAttribute("content", backgroundColor);
+    } else {
+      const meta = document.createElement('meta');
+      meta.name = "theme-color";
+      meta.content = backgroundColor;
+      document.head.appendChild(meta);
+    }
   }, [backgroundColor]);
 
   return backgroundColor;
